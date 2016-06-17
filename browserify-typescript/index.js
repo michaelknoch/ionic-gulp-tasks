d6bb9951bb8431b0e8595e6a1ae4ce79d8aacaac
Michael Knoch <info@michaelknoch.de>
2016-06-18 01:26:20 +0200

    transform relative urls to inline html

--- browserify-typescript/index.js
+++ browserify-typescript/index.js
@@ -8,7 +8,9 @@ var gulp = require('gulp'),
     buffer = require('vinyl-buffer'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
-    stream = require('stream');
+    stream = require('stream'),
+    through = require('through2'),
+    ng2TemplateParser = require('gulp-inline-ng2-template/parser');
 
 var defaultOptions = {
   watch: false,
@@ -31,13 +33,25 @@ var defaultOptions = {
   onLog: function(log){
     console.log((log = log.split(' '), log[0] = pretty(log[0]), log.join(' ')));
   }
-}
+};
 
 module.exports = function(options) {
   options = merge(defaultOptions, options);
 
   var b = browserify(options.src, options.browserifyOptions)
-    .plugin(tsify, options.tsifyOptions);
+    .plugin(tsify, options.tsifyOptions)
+      .transform(function (file) {
+          return through(function (buf, enc, next) {
+              ng2TemplateParser({contents: buf, path: file}, {
+                  base: '/app',
+                  useRelativePaths: true,
+                  supportNonExistentFiles: false
+              })((err, result) => {
+                  this.push(result);
+                  process.nextTick(next);
+              });
+          });
+      });
 
   if (options.watch) {
     b = watchify(b, options.watchifyOptions);
@@ -62,4 +76,4 @@ module.exports = function(options) {
   function noop(){
     return new stream.PassThrough({ objectMode: true });
   }
-}
+};
